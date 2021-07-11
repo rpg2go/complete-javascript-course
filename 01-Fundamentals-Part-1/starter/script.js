ab48a52bfc0d320004f8f0cff79e77cc985e6d9b
rpg2go <druga.marius@gmail.com>
2021-07-12 01:22:05 +0300

    commit changes

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,38 @@
+/*
+let javascriptisfun = true;
+console.log(javascriptisfun);
+
+console.log("typeof javascriptisfun is " + typeof javascriptisfun);
+
+console.log(typeof 23);
+
+console.log(typeof "Johas");
+
+javascriptisfun = "Yes";
+console.log("typeof javascriptisfun is " + typeof javascriptisfun);
+
+let myName;
+console.log(typeof myName);
+
+myName = null;
+console.log(typeof myName);
+console.log(typeof null);
+
+myName = "";
+console.log(typeof myName);
+
+*/
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+console.log(25 - 10 - 5);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
