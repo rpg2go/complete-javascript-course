f21bc937158bc63d25f8ff2f7d04592a7c6578b6
rpg2go <druga.marius@gmail.com>
2021-07-11 20:11:03 +0300

    add 1rst assignment scripts

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,6 @@
+var country='Romania';
+var continent='Europe';
+var population='12.57mil';
+
+alert(country + ' ' + continent + ' ' + population);
+console.log(country + ' ' + continent + ' ' + population);
