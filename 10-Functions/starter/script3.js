79f80c24a887eedeadf94deaf86f879b13acf79b
rpg2go <druga.marius@gmail.com>
2022-03-23 00:13:45 +0200

    challenge 02

--- 10-Functions/starter/script3.js
+++ 10-Functions/starter/script3.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const secureBooking = function () {
+    let passengerCount = 0;
+
+    return function () {
+        passengerCount++;
+        console.log(`${passengerCount} passenger`);
+    };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+booker();
+
+console.dir(booker);
