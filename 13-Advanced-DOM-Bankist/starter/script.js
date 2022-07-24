ff76e7809fed3b00382d6dd94bcf3c22dfdb9439
rpg2go <druga.marius@gmail.com>
2022-07-24 09:49:12 +0300

    finish advance dom and events

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -523,3 +523,30 @@ const slider = function () {
 };
 
 slider();
+
+//Lifecycle DOM events
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree build');
+});
+
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded, e');
+});
+
+// window.addEventListener('beforeunload', function (e) {
+//   e.preventDefault();
+//   console.log(e);
+
+//   e.returnValue = '';
+// });
+
+//Loading java script & HTML
+
+// regular
+// <script src="script.js"></script>
+
+// async
+// <script async src="script.js"></script>
+
+// defer
+// <script defer src="script.js"></script>
